050e0352a5d8cfc1a71e67a6ba51f17ac7addcad
Trevor Cunagin <trevorcunagin@gmail.com>
2021-03-17 23:20:48 -0400

    Added functionality for user to choose to reverse the list

--- sort.py
+++ sort.py
@@ -5,9 +5,17 @@ file2=open("Sorted.txt","w")
 with open("Sort Me.txt") as file1:
     file1list = file1.read().splitlines()
 
-#first sorts alphabetically, then by length 
+#sorts alphabetically
 file1list.sort()
-file1list.sort(key=len)
+
+#sorts by length either normally or in reverse depending on user input
+userChoice = ''
+while userChoice not in ['Y', 'y', 'N', 'n']:
+    userChoice = input("Do you want to reverse the list? Y/N: ")
+    if userChoice.upper() == 'Y':
+        file1list.sort(key=len, reverse=True)
+    elif userChoice.upper() == 'N':
+        file1list.sort(key=len)
 
 #writes items from sorted list into the Sorted.txt file
 for items in file1list:
